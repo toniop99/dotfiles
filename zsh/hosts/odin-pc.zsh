c56d4e39ac153b842e3b7388ab0394edbc1465f2
toniop <andezdev@gmail.com>
2024-08-26 18:46:36 +0200

    Add gpg for github

--- zsh/hosts/odin-pc.zsh
+++ zsh/hosts/odin-pc.zsh
@@ -4,3 +4,7 @@ if [[ $HOST == "odin" ]]; then
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fi
+
+PATH=$PATH:/usr/local/go/bin
+
+export GPG_TTY=$(tty)
